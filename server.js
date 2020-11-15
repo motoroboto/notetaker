5fdfe5e9f55fa38415eed1a0794b027969a20919
motoroboto <robert.a.cullen@gmail.com>
2020-11-15 14:00:18 -0500

    set up api get route

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 // Dependencies
-// =============================================================
 var express = require("express");
 var path = require("path");
+var PORT = process.env.PORT || 3000;
+var db = require('./db/db.json');
 
 // Sets up the Express App
-// =============================================================
 var app = express();
-var PORT = 3000;
-
-// Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static('public'));
 
 
-// Routes
+// HTML Routes
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "public/index.html"));
 });
 
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "public/notes.html"));
+});
+
+// API Routes
+app.get("/api/notes", function (req, res) {
+    return res.json(db);
+    // console.log('db:', db)
 });
 
 app.listen(PORT, function () {
