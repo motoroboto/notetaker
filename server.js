714a9722b9eeee686f8afd18358033b1ef0f5175
motoroboto <robert.a.cullen@gmail.com>
2020-11-21 13:39:19 -0500

    refacting code and have routes running without throwing errors mostly

--- server.js
+++ server.js
@@ -1,45 +1,37 @@
 // Dependencies
-var express = require("express");
-var path = require("path");
-var fs = require("fs");
-var PORT = process.env.PORT || 3000;
-var db = require('./db/db.json');
+const express = require("express");
+const app = express();
+const path = require("path");
+const fs = require("fs");
+let db = require('./db/db.json');
+let PORT = process.env.PORT || 3000;
+var newList = [{}];
 
 // Sets up the Express App
-var app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 
 // HTML Routes
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "public/index.html"));
-});
-function loadNotes() {
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "public/notes.html"));
-    });
-};
-
-app.get("/api/notes", function (req, res) {
-    return res.json(db);
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "public/notes.html"));
 });
 
 // API Routes
+app.get("/api/notes", function (req, res) {
+    return res.json(db);
+});
 
 app.post("/api/notes", function (req, res) {
     var newNote = req.body;
     var data = fs.readFileSync('./db/db.json');
     var json = JSON.parse(data);
     json.push(newNote);
-    console.log('json:', json)
-    const indexed = json.map((json, id) => Object.assign({ id }, json,))
-    console.log(indexed)
-    fs.writeFile("./db/db.json", JSON.stringify(indexed), function (err, result) {
-        if (err) console.log('error', err);
-    });
-    loadNotes();
+    json.forEach(function (json) { delete json.id });
+    newList = json.map((json, id) => Object.assign({ id }, json,))
+    renderList();
+    res.send(`Successfully added an entry!`)
 });
 
 app.delete("/api/notes/:id", function (req, res) {
@@ -47,17 +39,21 @@ app.delete("/api/notes/:id", function (req, res) {
     console.log('target:', deleteId)
     var data = fs.readFileSync('./db/db.json');
     var json = JSON.parse(data);
-    var newData = json.filter(function (object) {
+    newList = json.filter(function (object) {
         return object.id != deleteId;
-    })
-    fs.writeFile("./db/db.json", JSON.stringify(newData), function (err, result) {
-        if (err) console.log('error', err);
     });
-    loadNotes();
-
+    renderList();
+    res.send("Successfully deleted an entry!");
 });
 
+// Render
+function renderList() {
+    fs.writeFile("./db/db.json", JSON.stringify(newList), function (err, res) {
+        if (err) console.log('error', err);
+    });
+};
+
+// Server Listening
 app.listen(PORT, function () {
     console.log("App listening on PORT " + PORT);
-    loadNotes()
 });
